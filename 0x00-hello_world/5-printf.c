8da901c9837993f63d63ed294f48b2f05ca4b3d8
Hazel <hazellarkai26@gmail.com>
2023-09-07 19:04:25 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints a specific string
+ * with proper grammar using printf.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
