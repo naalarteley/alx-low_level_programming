b7dfbfc75075aa8218a78e1a77c0dd0a42903f84
Hazel <hazellarkai26@gmail.com>
2023-09-07 18:57:41 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that prints a string
+ * and returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+	/* Use the puts function to print the desired string */
+	puts("Programming is like building a multilingual puzzle");
+
+	/* Return 0 to indicate successful execution */
+	return (0);
 }
 
