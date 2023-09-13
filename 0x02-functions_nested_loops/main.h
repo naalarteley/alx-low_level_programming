1dada0407ce32abc7d70efcf537dc1bc13225c5d
Hazel <hazellarkai26@gmail.com>
2023-09-12 18:11:44 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,14 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 #ifndef MAIN_H
-#ifdef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+/* Additional function prototypes or declarations can go here */
+
+#endif /* MAIN_H */
+
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
