069c7a9627315fcdbbf8da9b4083559179dd323b
Hazel <hazellarkai26@gmail.com>
2023-09-15 23:08:36 -0700

    Hmmm

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * This function tests the `positive_or_negative` function
+ * by passing it the integer value 98.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    i = 98;
+    positive_or_negative(i);
+
+    return (0);
+}
+
