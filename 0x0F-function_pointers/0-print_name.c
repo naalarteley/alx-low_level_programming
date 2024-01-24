de62f98e31c5c6943ba52a308318d0d31eb3eef8
Hazel <hazellarkai26@gmail.com>
2024-01-24 06:30:07 -0800

    fuction pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - a fuction that prints a nae
+ * @f: variable to hold string name
+ * @name: name of person
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL) 
+		return;
+
+	f(name);
+}
