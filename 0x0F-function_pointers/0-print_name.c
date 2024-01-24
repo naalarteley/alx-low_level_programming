848ce780002581474679207e2b9eb298e7608e51
Hazel <hazellarkai26@gmail.com>
2024-01-24 07:16:15 -0800

    . What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,12 @@
 #include "function_pointers.h"
 /**
- * print_name - a fuction that prints a nae
+ * print_name - a function that prints a name
  * @f: variable to hold string name
  * @name: name of person
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL) 
+	if (name == NULL || f == NULL)
 		return;
-
-	f(name);
+	(*f)(name);
 }
